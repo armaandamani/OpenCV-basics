e8514344775fddc4e715bde90c8691b2205c1076
Armaan Damani <armaandamani@gmail.com>
2023-09-27 02:45:08 -0700

    openCV_project.py

--- openCV_project.py
+++ openCV_project.py
@@ -42,7 +42,7 @@ def loadImages(directory):
             gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
             blurred = cv2.GaussianBlur(gray, (3, 3), 0)
             edged = cv2.Canny(blurred, 45, 90)
-            cnts, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = own.contours(cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE))
             images.append(resized)
             contours.append(cnts)
     return images, contours #Returns a list of the images and their derived contours from the specified directory
