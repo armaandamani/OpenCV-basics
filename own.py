2260ae4874ef60943f930b45763f0d23abbc11a3
Armaan Damani <armaandamani@gmail.com>
2025-02-24 15:00:49 -0800

    own.py

--- own.py
+++ own.py
@@ -1,34 +1,72 @@
 import numpy as np
 import cv2
 
-def translate(image, x, y):
-    M = np.float32([[1, 0, x], [1, 0, y]])
-    shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
-    return shifted
-
-def rotate(image, degrees, scale):
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-
-    M = cv2.getRotationMatrix2D(center, degrees, scale)
-    rotated = cv2.warpAffine(image, M, (w, h))
-    return rotated
-
-def resize(image, width = None, height = None):
-    (h, w) = image.shape[:2]
-
-    if width == None and height == None:
-        return image
-    elif width == None:
-        r = height / h
-        dim = (int(w*r), int(height))
-        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        return resized
-    elif height == None:
-        r = width / w
-        dim = (int(width), int(h*r))
-        resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        return resized
+def translate_image(image, dx, dy):
+    """Translate the image by dx and dy pixels horizontally and vertically, respectively.
     
-def contours(cnts):
-    return cnts[0]
+    Parameters:
+    - image: The input image to be translated.
+    - dx: The number of pixels to shift the image horizontally (positive for right, negative for left).
+    - dy: The number of pixels to shift the image vertically (positive for down, negative for up).
+    
+    Returns:
+    - The translated image.
+    """
+    translation_matrix = np.float32([[1, 0, dx], [0, 1, dy]])
+    shifted_image = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]))
+    return shifted_image
+
+def rotate_image(image, angle_degrees, scale_factor):
+    """Rotate the image by a specified angle in degrees with a given scale factor.
+    
+    Parameters:
+    - image: The input image to be rotated.
+    - angle_degrees: The rotation angle in degrees (positive for counterclockwise, negative for clockwise).
+    - scale_factor: The scaling factor for the rotation.
+    
+    Returns:
+    - The rotated image.
+    """
+    height, width = image.shape[:2]
+    center = (width // 2, height // 2)
+    rotation_matrix = cv2.getRotationMatrix2D(center, angle_degrees, scale_factor)
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+    return rotated_image
+
+def resize_image(image, new_width=None, new_height=None):
+    """Resize the image to the specified width and/or height while maintaining the aspect ratio if only one is provided.
+    
+    Parameters:
+    - image: The input image to be resized.
+    - new_width: The desired width of the image (optional).
+    - new_height: The desired height of the image (optional).
+    
+    Returns:
+    - The resized image.
+    """
+    height, width = image.shape[:2]
+    
+    if new_width is None and new_height is None:
+        return image
+    elif new_width is None:
+        aspect_ratio = new_height / height
+        new_dimensions = (int(width * aspect_ratio), new_height)
+    elif new_height is None:
+        aspect_ratio = new_width / width
+        new_dimensions = (new_width, int(height * aspect_ratio))
+    else:
+        new_dimensions = (new_width, new_height)
+    
+    resized_image = cv2.resize(image, new_dimensions, interpolation=cv2.INTER_AREA)
+    return resized_image
+
+def extract_contours(find_contours_output):
+    """Extract the list of contours from the output of cv2.findContours.
+    
+    Parameters:
+    - find_contours_output: The output tuple from cv2.findContours.
+    
+    Returns:
+    - The list of contours.
+    """
+    return find_contours_output[0]
